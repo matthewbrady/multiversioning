55535905b1914c00f84b7592f7aa1ba9f555f871
Stan <stanislaw.swidwinski@yale.edu>
2017-03-21 11:05:20 -0400

    More documentation

--- include/batch/input_queue.h
+++ include/batch/input_queue.h
@@ -18,6 +18,11 @@ class Scheduler;
  *    on to the scheduling threads in round-robbin manner. Notice that this means
  *    that the ordering of scheduling threads does not change (i+1 st thread will
  *    get a batch of actions after ith thread. Always.)
+ *
+ *    The input queue is a one-producer, one-consumer MS queue. The implementation
+ *    makes sure that only one scheduler may dequeue transactions at any point in time.
+ *    Enqueueing is done by the simulation framework, which ensures that only one thread
+ *    enqueues at a time.
  */
 class InputQueue : public MSQueue<std::unique_ptr<BatchAction>> {
   private:
--- include/batch/scheduler.h
+++ include/batch/scheduler.h
@@ -33,26 +33,32 @@ enum class SchedulerState {
   state_count
 };
 
-// TODO:
-//    - Resolve the issue of a lifecycle of a transaction. Should we have a
-//      single producer/single consumer input queue with a counter whose
-//      mod # of scheduling threads dictates which thread is handling a batch?
-//      Should we have a direct way of enqueuing to a given scheduling thread?
-//      The latter seems like a cheat, the former might be a bottle neck, but perhaps
-//      not so much.
+//  Scheduler
 //
-//      For now I will go with a global input queue implemented in "batch/input_queue.h".
-//      Threads will pass the lock on the input queue among themselves using CAS so that
-//      not much contention is to be expected.
+//    Implementation of the scheduling thread. Scheduling threads may be 
+//    thought of as state machines. The state transitions are described below.
 //
 //    State transition of scheduler:
 //      - waiting_for_input 
+//          -- Awaits its turn to dequeue actions from the input queue. Input
+//            Queue is a single-producer single-consumer MS-queue that contains 
+//            all of the transactions in the system.
 //      - input 
+//          -- Dequeues transactions from the input queue.
 //      - batch_creation 
+//          -- Creates the batch schedule. This is done offline and does not
+//          conflict with anything else in the system.
 //      - waiting to merge 
+//          -- Awaits for the logical lock on merging its schedule into the 
+//          global schedule.
 //      - batch merging
+//          -- Merges into the global schedule.
 //      - waiting to signal execution
+//          -- Awaits for the logical lock on signaling to execution threads.
+//          Signaling execution threads means passing them pointers to
+//          actions that they own. For more information see include/batch/executor.h
 //      - signaling execution
+//          -- Signals execution threads
 //      - waiting for input
 //      - ...
 //
